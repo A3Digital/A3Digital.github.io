dfd0e6890b3453c5913d22eaba4233993bb40419
Alayn Arambula <a3_digital@hotmail.com>
2017-12-11 04:11:59 +0000

    updating controls

--- Scourge/movement.js
+++ Scourge/movement.js
@@ -1,14 +1,14 @@
 function movement() {
-    if(keyDown(87)) {
+    if(keyDown(87) || keyDown(UP_ARROW)) {
         yPos = constrain(yPos-1.5,0,400);
     }
-    if(keyDown(65)) {
+    if(keyDown(65) || keyDown(LEFT_ARROW)) {
         xPos = constrain(xPos-1.5,0,400);
     }
-    if(keyDown(83)) {
+    if(keyDown(83) || keyDown(DOWN_ARROW)) {
         yPos = constrain(yPos+1.5,0,400);
     }
-    if(keyDown(68)) {
+    if(keyDown(68) || keyDown(RIGHT_ARROW)) {
         xPos = constrain(xPos+1.5,0,400);
     }
 }
